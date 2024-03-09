bdf952486ca5f46551a275fd903eff1345cca78b
victorcosmedev <victorcosme.profissional@gmail.com>
2024-03-08 22:36:14 -0300

    aula02

---
+++ aula02.py
@@ -0,0 +1,27 @@
+nome = "Antonio"
+idade = 25
+peso = 75.5
+altura = 1.80
+usuario_python = True
+endereco = "Avenida Paulista, 1313"
+pcd = False
+# data_de_nascimento
+dia_de_nascimento = 15
+mes_de_nascimento = 10
+ano_de_nascimento = 2000
+
+print(nome)
+print(idade)
+print(peso)
+print(altura)
+print(usuario_python)
+print(endereco)
+print(pcd)
+print(dia_de_nascimento)
+print(mes_de_nascimento)
+print(ano_de_nascimento)
+
+print(endereco[-3:])
+
+# Retorna uma lista de substrings com um determinado separador, nesse caso, a vírgula
+print(endereco.split(','))
